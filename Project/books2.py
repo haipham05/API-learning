9b58abbde3b38b3fbae3a574f963854ecf04c052
nope0000 <haidongmvp@gmail.com>
2025-09-19 08:16:20 +0700

    2nd commit

---
+++ Project/books2.py
@@ -0,0 +1,79 @@
+from typing import Optional
+from fastapi import FastAPI, Body
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str 
+    rating: int
+
+    def __init__(self, id, title, author, description, rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+
+class BookRequest(BaseModel):
+    id: Optional[int] = Field(description='ID is not needed on create', default=None)
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                'title': 'A new book',
+                'author': 'HaiPham',
+                'description': 'A description',
+                'rating': 5
+            }
+        }
+    }
+
+BOOKS = [
+    Book(1, "Comp-Sci", "Hai Pham", "Comp-Sci fundamental", 5),
+    Book(2, "AI don gian", "Hai Pham", "very gud book", 5),
+    Book(3, "The Lord of the Rings", "J.R.R.Tolkien", "peak fantasy novel", 5),
+    Book(4, "God Father", "Mario Puzo", "Great book", 5),
+    Book(5, "Toi thay hoa vang tren co xanh ", "Nguyen Nhat Anh", "good book to read", 4),
+    Book(6, "Rich Dad Poor Dad", "Robert T.Kiyosaki", "groundbreaking personal finance book", 4),
+]
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
+
+
+@app.get("/books/{book_id}")
+async def read_book(book_id: int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+
+@app.get("/books/")
+async def read_book_by_rating(rating: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating == rating:
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.put("/books/update_book")
+async def update_book(book: BookRequest):
+    
+@app.post("/create-book")
+async def create_book(book_request: BookRequest):
+    newbook = Book(**book_request.model_dump())
+    BOOKS.append(find_book_id(newbook))
+
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    return book
+
